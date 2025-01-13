9eadadbd4ce1c79f79c1766a889ba732117a0195
ALOUDDIN9606 <xonimqulovalouddin@gmail.com>
2025-01-13 14:41:51 +0500

    new

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { User } from './user/models/user.model';
 import { AuthModule } from './auth/auth.module';
 import { Otp } from './auth/models/otp.model';
 import { MailModule } from './mail/mail.module';
+import { DiscountModule } from './discount/discount.module';
 
 @Module({
   imports: [
@@ -28,6 +29,7 @@ import { MailModule } from './mail/mail.module';
     UserModule,
     AuthModule,
     MailModule,
+    DiscountModule,
   ],
   controllers: [],
   providers: [],
