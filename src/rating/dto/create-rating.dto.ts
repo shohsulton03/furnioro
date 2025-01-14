b00df13ce6717878fe615a6339ea92b56becf3c9
Fakhriddin <mavlonovfahriddin0822@gmail.com>
2025-01-15 01:06:44 +0500

    rating

---
+++ src/rating/dto/create-rating.dto.ts
@@ -0,0 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateRatingDto {
+  @ApiProperty({
+    example: 1,
+    description: 'ID of product',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  product_id: number;
+
+  @ApiProperty({
+    example: 1,
+    description: 'ID of user',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  user_id: number;
+
+  @ApiProperty({
+    example: 1,
+    description: 'Product rating',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  raiting: number;
+
+  @ApiProperty({
+    example: 1,
+    description: 'Review count',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  review_count: number;
+}
