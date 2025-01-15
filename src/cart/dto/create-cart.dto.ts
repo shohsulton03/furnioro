aa3af3dc0861cd62c986067e2dcda81f43ec011b
shohsulton03 <shohsulton0303@mail.ru>
2025-01-15 13:32:37 +0500

    fixed cart

--- src/cart/dto/create-cart.dto.ts
+++ src/cart/dto/create-cart.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class CreateCartDto {
     
@@ -9,13 +9,6 @@ export class CreateCartDto {
         example: 23,
         description: "ID of user"
     })
+    @IsNumber()
     user_id: number;
-
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty({
-        example: "2025-01-14T10:30:00.000Z",
-        description: "Date and time when the record was created (ISO format)",
-    })
-    createdAt: Date;
 }
